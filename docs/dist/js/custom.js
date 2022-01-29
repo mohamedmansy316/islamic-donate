bdd00721af5622ea27d2b899b9d5eb4ec71e42f4
mohamedmansy316 <mohamedmansy855@gmail.com>
2022-01-30 00:52:36 +0200

    add

---
+++ docs/dist/js/custom.js
@@ -0,0 +1,31 @@
+
+mybutton = document.getElementById("myBtn");
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
+
+let loader = document.querySelector('.loader');
+function Loader() {
+  setTimeout(function(){ 
+    loader.style.visibility = 'hidden';
+    loader.style.opacity = 0;
+    loader.style.transition = "all 1s";
+    document.body.style.height = "auto";
+    document.getElementsByTagName('html')[0].style.overflowX = "hidden";
+    document.getElementsByTagName('html')[0].style.overflowY = "auto";
+  }, 3000);
+}
+Loader();
